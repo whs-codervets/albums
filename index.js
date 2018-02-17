946d24ed42d84a87e8af73a89b2b78320c1155b1
William Smith <whs2p8@mail.umsl.edu>
2018-02-17 12:22:02 -0600

    moved files

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Import a library to help create a component
 import React from 'react';
+
 import { AppRegistry, View } from 'react-native';
 import Header from './src/components/Header';
 import AlbumList from './src/components/AlbumList';
